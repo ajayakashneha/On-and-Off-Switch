ebe6f0a947ec404c754d8375352d8b54d98b792a
Neha Kumari <nehaatwork23@gmail.com>
2023-10-03 17:08:17 +0530

    bulb-on and bulb-offo

--- bulb.js
+++ bulb.js
@@ -1,15 +1,20 @@
 var buttonOff = document.querySelector("#button-off")
-let com=0
+let com=0;
 buttonOff.addEventListener("click", function () {
+
     if(com===0){
         buttonOff.textContent = "TURN ON";
         com =1
-        buttonOff.style.backgroundColor="green"
+        buttonOff.style.backgroundColor = "green"
+        let img = document.querySelector("#bulbImage")
+        img.setAttribute("src" , "bulb-on.png")
        }
        else{
         buttonOff.textContent = "TURN Off";
         com =0
         buttonOff.style.backgroundColor="white"
+        let img = document.querySelector("#bulbImage")
+        img.setAttribute("src" , "bulb-off.png")
     }
 });
 
